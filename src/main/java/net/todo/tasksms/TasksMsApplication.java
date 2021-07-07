97845ab443ab4c8ed3b14d723ff8588b85f12a84
ssj256x <ssj256x@outlook.com>
2021-07-07 22:36:24 +0530

    added initial codebase

---
+++ src/main/java/net/todo/tasksms/TasksMsApplication.java
@@ -0,0 +1,13 @@
+package net.todo.tasksms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksMsApplication.class, args);
+	}
+
+}
